52dbb19b57a83ddc31a59d31d3fd17b95030b6c8
Hincoin <lucenadeveloper@gmail.com>
2014-03-26 22:56:44 -0400

    cleaned up  message_queue.cpp, removed main and examples

--- message_queue.cpp
+++ message_queue.cpp
@@ -10,10 +10,10 @@ template<class ReturnType,class Ident, class... Arguments>
 class message_queue
 {
 	private:
-	//	std::thread reading;
+
 		std::thread executing;
 		std::queue<std::tuple<std::function<ReturnType(Arguments...)>,Ident, Arguments...>> job_queue;
-	//	std::queue<ReturnType> return_values; 
+
 		std::unordered_map<Ident,ReturnType> return_values;
 		std::unordered_map<Ident,bool> added;
 		std::mutex retrieval_lock;
@@ -82,9 +82,6 @@ class message_queue
 			isDone = true;
 			added.clear();
 			return_values.clear();
-		
-	//		if(reading != nullptr && reading.joinable())
-	//			reading.join();
 			if(executing.joinable())
 				executing.join();
 
@@ -94,41 +91,4 @@ class message_queue
 
 
 };
-int someFunction(std::tuple<int,int> tup)
-{
-	return 6445 * 121 + 12231 / 43441 * 2124 - 542324 + 123442 - 5 / 212 + 47;
 
-
-}
-int moreMath(std::tuple<int,int> tup)
-{
-	return someFunction(tup) / 121 + 3 * 223 - std::get<0>(tup) + std::get<1>(tup);
-	
-
-}
-int simpleMath(std::tuple<int,int> tup)
-{
-	return 16 * std::get<0>(tup) + std::get<1>(tup);
-
-}
-
-int main()
-{
-	message_queue<int,int,std::tuple<int,int>> m_queue;
-	std::function<int(std::tuple<int,int>)> f1 = someFunction;
-	std::function<int(std::tuple<int,int>)> f2 = moreMath;
-	std::function<int(std::tuple<int,int>)> f3 = simpleMath;
-	m_queue.add(f1,1,std::make_tuple(1,5));
-	m_queue.add(f2,2,std::make_tuple(1,5));
-	m_queue.add(f3,3,std::make_tuple(1,5));
-//	for(int j = 0; j < 50 ; ++j)
-	for(int i =0; i < 3; ++i)
-	{
-		std::cout << "Getting id: " << (i+1 ) << "  " <<  m_queue.get(i+1) << "\n";
-
-	}
-	
-
-
-	return 0;
-}
